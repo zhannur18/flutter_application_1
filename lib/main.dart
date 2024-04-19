06ba0eb4e63921b3865405c49d79f5f2a3e4512a
zhannur18 <129687473+zhannur18@users.noreply.github.com>
2024-04-19 14:01:05 +0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,89 +1,23 @@
 import 'package:flutter/material.dart';
-import 'package:wave/config.dart';
-import 'package:wave/wave.dart';
-import 'package:flutter_villains/villains/villains.dart'; // Import the package
+import 'package:shop_app/constants.dart';
+import 'package:shop_app/screens/home/home_screen.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: WavePage(),
-    );
-  }
-}
-
-class WavePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('WaveS'),
+      debugShowCheckedModeBanner: false,
+      title: 'The Flutter Way',
+      theme: ThemeData(
+        textTheme: Theme.of(context).textTheme.apply(bodyColor: kTextColor),
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      body: Stack(
-        children: [
-          WaveWidget(
-            config: CustomConfig(
-              gradients: [
-                [Colors.greenAccent, Colors.green.shade300],
-                [Colors.blue.shade500, Colors.blue.shade200],
-              ],
-              durations: [35000, 19440],
-              heightPercentages: [0.16, 0.22],
-              gradientBegin: Alignment.bottomLeft,
-              gradientEnd: Alignment.topRight,
-            ),
-            waveAmplitude: 0,
-            size: Size(
-              double.infinity,
-              double.infinity,
-            ),
-          ),
-          Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  'Keep calm and relax',
-                  style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
-                ),
-                SizedBox(height: 20),
-                ElevatedButton(
-                  onPressed: () {
-                    // Navigate to the second screen with a transition effect
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (_) => SecondScreen()));
-                  },
-                  child: Text('sample button'),
-                ),
-              ],
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class SecondScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Villain(
-      villainAnimation: VillainAnimation.fade(),
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text('Second Screen'),
-        ),
-        body: Center(
-          child: Text(
-            'Welcome to the second screen!',
-            style: TextStyle(fontSize: 20),
-          ),
-        ),
-      ),
+      home: HomeScreen(),
     );
   }
 }
